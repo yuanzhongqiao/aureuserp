b87446115006c764e9c0ded9abf1022bae026ed6
Jitendra <jitendra@webkul.in>
2025-02-13 19:16:51 +0530

    Move product resources to products plugin

---
+++ plugins/webkul/products/resources/lang/en/filament/resources/category.php
@@ -0,0 +1,116 @@
+<?php
+
+return [
+    'form' => [
+        'sections' => [
+            'general' => [
+                'title'  => 'General',
+
+                'fields' => [
+                    'name'             => 'Name',
+                    'name-placeholder' => 'eg. Lamps',
+                    'parent'           => 'Parent',
+                ],
+            ],
+
+            'settings' => [
+                'title'  => 'Settings',
+
+                'fieldsets' => [
+                    'logistics' => [
+                        'title' => 'Logistics',
+
+                        'fields' => [
+                            'routes' => 'Routes',
+                        ],
+                    ],
+
+                    'inventory-valuation' => [
+                        'title' => 'Inventory Valuation',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'table' => [
+        'columns' => [
+            'name'        => 'Name',
+            'full-name'   => 'Full Name',
+            'parent-path' => 'Parent Path',
+            'parent'      => 'Parent',
+            'creator'     => 'Creator',
+            'created-at'  => 'Created At',
+            'created-at'  => 'Created At',
+            'updated-at'  => 'Updated At',
+        ],
+
+        'groups' => [
+            'parent'     => 'Parent',
+            'created-at' => 'Created At',
+            'updated-at' => 'Updated At',
+        ],
+
+        'filters' => [
+            'parent' => 'Parent',
+        ],
+
+        'actions' => [
+            'delete' => [
+                'notification' => [
+                    'title' => 'Category deleted',
+                    'body'  => 'The Category has been deleted successfully.',
+                ],
+            ],
+        ],
+
+        'bulk-actions' => [
+            'delete' => [
+                'notification' => [
+                    'title' => 'Categories deleted',
+                    'body'  => 'The categories has been deleted successfully.',
+                ],
+            ],
+        ],
+    ],
+
+    'infolist' => [
+        'sections' => [
+            'general' => [
+                'title' => 'General Information',
+
+                'entries' => [
+                    'name'        => 'Name',
+                    'parent'      => 'Parent Category',
+                    'full_name'   => 'Full Category Name',
+                    'parent_path' => 'Category Path',
+                ],
+            ],
+
+            'settings' => [
+                'title' => 'Settings',
+
+                'subsections' => [
+                    'logistics' => [
+                        'title' => 'Logistics',
+
+                        'entries' => [
+                            'routes'     => 'Warehouse Routes',
+                            'route_name' => 'Route Name',
+                        ],
+                    ],
+                ],
+            ],
+
+            'record-information' => [
+                'title' => 'Record Information',
+
+                'entries' => [
+                    'creator'    => 'Created By',
+                    'created_at' => 'Created At',
+                    'updated_at' => 'Last Updated At',
+                ],
+            ],
+        ],
+    ],
+];
---
+++ plugins/webkul/products/resources/lang/en/filament/resources/packaging.php
@@ -0,0 +1,111 @@
+<?php
+
+return [
+    'form' => [
+        'name'    => 'Name',
+        'barcode' => 'Barcode',
+        'product' => 'Product',
+        'routes'  => 'Routes',
+        'qty'     => 'Qty',
+        'company' => 'Company',
+    ],
+
+    'table' => [
+        'columns' => [
+            'name'       => 'Name',
+            'product'    => 'Product',
+            'routes'     => 'Routes',
+            'qty'        => 'Qty',
+            'company'    => 'Company',
+            'barcode'    => 'Barcode',
+            'created-at' => 'Created At',
+            'updated-at' => 'Updated At',
+        ],
+
+        'groups' => [
+            'product'    => 'Product',
+            'created-at' => 'Created At',
+            'updated-at' => 'Updated At',
+        ],
+
+        'filters' => [
+            'product' => 'Product',
+        ],
+
+        'actions' => [
+            'edit' => [
+                'notification' => [
+                    'title' => 'Packaging update',
+                    'body'  => 'The packaging has been update successfully.',
+                ],
+            ],
+
+            'delete' => [
+                'notification' => [
+                    'title' => 'Packaging deleted',
+                    'body'  => 'The packaging has been deleted successfully.',
+                ],
+            ],
+        ],
+
+        'bulk-actions' => [
+            'print' => [
+                'label' => 'Print',
+            ],
+            
+            'delete' => [
+                'notification' => [
+                    'title' => 'Packagings deleted',
+                    'body'  => 'The packagings has been deleted successfully.',
+                ],
+            ],
+        ],
+
+        'empty-state-actions' => [
+            'create' => [
+                'label' => 'New Packaging',
+
+                'notification' => [
+                    'title' => 'Packaging created',
+                    'body'  => 'The packaging has been created successfully.',
+                ],
+            ],
+        ],
+    ],
+
+    'infolist' => [
+        'sections' => [
+            'general' => [
+                'title' => 'General Information',
+
+                'entries' => [
+                    'name'         => 'Package Name',
+                    'barcode'      => 'Barcode',
+                    'product'      => 'Product',
+                    'qty'          => 'Quantity',
+                    'package_type' => 'Package Type',
+                ],
+            ],
+
+            'routing' => [
+                'title' => 'Routing Information',
+
+                'entries' => [
+                    'routes'     => 'Warehouse Routes',
+                    'route_name' => 'Route Name',
+                ],
+            ],
+
+            'organization' => [
+                'title' => 'Organization Details',
+
+                'entries' => [
+                    'company'    => 'Company',
+                    'creator'    => 'Created By',
+                    'created_at' => 'Created At',
+                    'updated_at' => 'Last Updated At',
+                ],
+            ],
+        ],
+    ],
+];
