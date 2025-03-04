de370a0c1354593a83fdc1ce5a5c10222efed2be
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-03-04 17:21:08 +0530

    Enhance refund editing with improved data handling and notifications

--- plugins/webkul/accounts/src/Filament/Resources/RefundResource/Pages/EditRefund.php
+++ plugins/webkul/accounts/src/Filament/Resources/RefundResource/Pages/EditRefund.php
@@ -3,14 +3,30 @@
 namespace Webkul\Account\Filament\Resources\RefundResource\Pages;
 
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 use Webkul\Account\Filament\Resources\InvoiceResource\Actions as BaseActions;
 use Webkul\Account\Filament\Resources\RefundResource;
+use Webkul\Account\Models\Partner;
 use Webkul\Chatter\Filament\Actions as ChatterActions;
 
 class EditRefund extends EditRecord
 {
     protected static string $resource = RefundResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title(__('accounts::filament/resources/refund/pages/edit-refund.notification.title'))
+            ->body(__('accounts::filament/resources/refund/pages/edit-refund.notification.body'));
+    }
 
     protected function getHeaderActions(): array
     {
@@ -24,5 +40,37 @@ class EditRefund extends EditRecord
             BaseActions\SetAsCheckedAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $user = Auth::user();
+
+        $record = $this->getRecord();
+
+        $data['partner_id'] ??= $record->partner_id;
+        $data['invoice_date'] ??= $record->invoice_date;
+        $data['name'] ??= $record->name;
+        $data['auto_post'] ??= $record->auto_post;
+        $data['invoice_currency_rate'] ??= 1.0;
+
+        if ($data['partner_id']) {
+            $partner = Partner::find($data['partner_id']);
+
+            $data['commercial_partner_id'] = $partner->id;
+            $data['partner_shipping_id'] = $partner->addresses->where('type', 'present')->first()?->id;
+            $data['invoice_partner_display_name'] = $partner->name;
+        } else {
+            $data['invoice_partner_display_name'] = "#Created By: {$user->name}";
+        }
+
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        $record = $this->getRecord();
+
+        $this->getResource()::collectTotals($record);
     }
 }
