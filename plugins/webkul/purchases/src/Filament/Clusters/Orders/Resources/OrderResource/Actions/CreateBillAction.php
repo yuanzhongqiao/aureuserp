ed3d9cc1bfc6f2e3ac09497f6d31d41d6e0c2dde
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-03-04 19:21:29 +0530

    fix the null credit, debit column into move lines from order bill generation.

--- plugins/webkul/purchases/src/Filament/Clusters/Orders/Resources/OrderResource/Actions/CreateBillAction.php
+++ plugins/webkul/purchases/src/Filament/Clusters/Orders/Resources/OrderResource/Actions/CreateBillAction.php
@@ -4,15 +4,15 @@ namespace Webkul\Purchase\Filament\Clusters\Orders\Resources\OrderResource\Actio
 
 use Filament\Actions\Action;
 use Filament\Notifications\Notification;
-use Webkul\Purchase\Models\AccountMove;
-use Webkul\Account\Models\Journal as AccountJournal;
-use Webkul\Purchase\Models\Order;
-use Webkul\Purchase\Enums\OrderState;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Webkul\Account\Enums as AccountEnums;
-use Webkul\Purchase\Filament\Clusters\Orders\Resources\OrderResource;
+use Webkul\Account\Models\Journal as AccountJournal;
 use Webkul\Invoice\Filament\Clusters\Vendors\Resources\BillResource;
+use Webkul\Purchase\Enums\OrderState;
+use Webkul\Purchase\Filament\Clusters\Orders\Resources\OrderResource;
+use Webkul\Purchase\Models\AccountMove;
+use Webkul\Purchase\Models\Order;
 
 class CreateBillAction extends Action
 {
@@ -66,21 +66,21 @@ class CreateBillAction extends Action
     private function createAccountMove($record): void
     {
         $accountMove = AccountMove::create([
-            'state' => AccountEnums\MoveState::DRAFT,
-            'move_type' => AccountEnums\MoveType::IN_INVOICE,
-            'payment_state' => AccountEnums\PaymentStatus::NOT_PAID,
+            'state'                        => AccountEnums\MoveState::DRAFT,
+            'move_type'                    => AccountEnums\MoveType::IN_INVOICE,
+            'payment_state'                => AccountEnums\PaymentStatus::NOT_PAID,
             'invoice_partner_display_name' => $record->partner->name,
-            'invoice_origin' => $record->name,
-            'date' => now(),
-            'invoice_date_due' => now(),
-            'invoice_currency_rate' => 1,
-            'journal_id' => AccountJournal::where('type', AccountEnums\JournalType::PURCHASE->value)->first()?->id,
-            'company_id' => $record->company_id,
-            'currency_id' => $record->currency_id,
-            'invoice_payment_term_id' => $record->payment_term_id,
-            'partner_id' => $record->partner_id,
-            'commercial_partner_id' => $record->partner_id,
-            'partner_shipping_id' => $record->partner_shipping_id,
+            'invoice_origin'               => $record->name,
+            'date'                         => now(),
+            'invoice_date_due'             => now(),
+            'invoice_currency_rate'        => 1,
+            'journal_id'                   => AccountJournal::where('type', AccountEnums\JournalType::PURCHASE->value)->first()?->id,
+            'company_id'                   => $record->company_id,
+            'currency_id'                  => $record->currency_id,
+            'invoice_payment_term_id'      => $record->payment_term_id,
+            'partner_id'                   => $record->partner_id,
+            'commercial_partner_id'        => $record->partner_id,
+            'partner_shipping_id'          => $record->partner_shipping_id,
             // 'partner_bank_id' => $record->partner_bank_id,//TODO: add partner bank id
             'fiscal_position_id' => $record->fiscal_position_id,
             // 'preferred_payment_method_line_id' => 1,
@@ -103,6 +103,8 @@ class CreateBillAction extends Action
             'name'                   => $orderLine->name,
             'display_type'           => AccountEnums\DisplayType::PRODUCT,
             'date'                   => $accountMove->date,
+            'creator_id'             => $accountMove?->creator_id,
+            'parent_state'           => $accountMove->state,
             'quantity'               => $orderLine->qty_to_invoice,
             'price_unit'             => $orderLine->price_unit,
             'discount'               => $orderLine->discount,
@@ -114,6 +116,10 @@ class CreateBillAction extends Action
             'product_id'             => $orderLine->product_id,
             'uom_id'                 => $orderLine->uom_id,
             'purchase_order_line_id' => $orderLine->id,
+            'debit'                  => $orderLine?->price_subtotal,
+            'credit'                 => 0.00,
+            'balance'                => $orderLine?->price_subtotal,
+            'amount_currency'        => $orderLine?->price_subtotal,
         ]);
 
         $orderLine->qty_invoiced += $orderLine->qty_to_invoice;
